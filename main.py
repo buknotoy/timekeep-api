44ad6eea09616b80d821bc6f2602b02a0d363689
Alex Samson <asamson@backpaddock.com.au>
2018-12-07 15:47:51 +1000

    Added CORS

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
+from flask_cors import CORS
 import os
 
+
 app = Flask(__name__)
+CORS(app)
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'timekeeping.db')
@@ -13,7 +16,7 @@ ma = Marshmallow(app)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return "Hello World!!!"
 
 class costcentre(db.Model):
     code = db.Column(db.String(20), primary_key = True, unique = True )
@@ -57,7 +60,7 @@ def add_costcentre():
     db.session.add(new_costcentre)
     db.session.commit()
 
-    return  jsonify(new_costcentre.toJson())  #jsonify(new_costcentre)
+    return  jsonify(new_costcentre.toJson()) 
 
 #endpoint to update cost centre
 @app.route("/api/costcentre/<id>", methods=['PUT'])
@@ -85,7 +88,8 @@ def get_costcentre():
 #endpoint to get cost centre detail
 @app.route("/api/costcentre/<id>", methods=['GET'])
 def costcentre_detail(id):
-    lcostCentre = costcentre.query.get(id)
+    #lcostCentre = costcentre.query.get(id)
+    lcostCentre = costcentre.query.filter(costcentre.code.ilike(id)).first()
     return costcentre_schema.jsonify(lcostCentre)
 
 #endpoint to delete cost centre
